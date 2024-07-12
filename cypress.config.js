1905c00c7d9e9c47c065880992228a74ef08dada
santhadurai <sinbanathan@magmutual.com>
2024-07-12 19:12:31 +0530

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,6 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: "2vh9jg",
   defaultCommandTimeout: 10000,
   pageLoadTimeout: 80000,
   experimentalFetchPolyfill:true,
